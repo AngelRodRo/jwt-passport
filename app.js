c511de46f079619bfe5507ace0709d3bc2160b22
Angel <aleangrodriguez123@gmail.com>
2020-07-02 09:57:26 -0500

    remove old server init

--- app.js
+++ app.js
@@ -18,22 +18,6 @@ require('./auth/auth');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
