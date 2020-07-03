5f517cf2170c453297e01418cc9296310711fe52
Angel <aleangrodriguez123@gmail.com>
2020-07-02 19:36:56 -0500

    refactor v2

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hello World')
 });
 
 
