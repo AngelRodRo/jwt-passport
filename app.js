62aa808b6a4acd68aacad6d870c59729f4fe34dc
Angel <aleangrodriguez123@gmail.com>
2020-07-04 15:48:18 -0500

    refactor

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 mongoose.connect('mongodb://127.0.0.1:27017/passport-jwt', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-});
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
 
-mongoose.connection.on('error', error => console.log(error) );
+const app = express()
 
-require('./auth/auth');
+require('./auth/auth')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
-app.use(routes);
+app.use(bodyParser.json())
+app.use(routes)
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const PORT = 3000
+
+app.listen(PORT, function () {
+  console.log(`App listening on ${PORT}`)
+})
